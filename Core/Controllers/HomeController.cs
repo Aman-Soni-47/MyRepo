73f0b987865b0cc8f1fb141c27b95e3a056d886c
aman <aman.soni@47billion.com>
2019-10-25 12:49:35 +0530

    Changes in homecontroller

--- Core/Controllers/HomeController.cs
+++ Core/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Core.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            
 
             return View();
         }
